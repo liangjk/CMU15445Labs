efa1eb98ed37397cbe1bd7bf6ac6dd6305eb3647
liangjk <liang_jk@alu.ruc.edu.cn>
2024-04-17 20:24:22 -0700

    p1 checkpoint

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -119,3 +119,149 @@ void PageScheduler::Join() {
   }
 }
 }  // namespace bustub
+
+// #include "common/exception.h"
+// #include "storage/disk/disk_manager.h"
+// #include "storage/disk/disk_scheduler.h"
+
+// namespace bustub {
+
+// DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
+//   page_shedulers_.reserve(MAX_OUTSTANDING);
+
+//   // Spawn the background thread
+//   background_thread_.emplace([&] { StartWorkerThread(); });
+// }
+
+// DiskScheduler::~DiskScheduler() {
+//   // Put a `std::nullopt` in the queue to signal to exit the loop
+//   request_queue_.Put(std::nullopt);
+//   if (background_thread_.has_value()) {
+//     background_thread_->join();
+//   }
+//   for (const auto &pair : page_shedulers_) {
+//     pair.second->Join();
+//     delete (pair.second);
+//   }
+// }
+
+// void DiskScheduler::Schedule(DiskRequest r) { request_queue_.Put(std::move(r)); }
+
+// void DiskScheduler::StartWorkerThread() {
+//   while (true) {
+//     auto req = request_queue_.Get();
+//     if (req.has_value()) {
+//       auto pid = req->page_id_;
+//       auto it = page_shedulers_.find(pid);
+//       if (it != page_shedulers_.end()) {
+//         if (it->second->Join()) {
+//           delete (it->second);
+//           it->second = new PageScheduler(std::move(*req), disk_manager_, current_++);
+//         } else {
+//           it->second->Schedule(std::move(*req), current_++);
+//         }
+//       } else {
+//         AddPageScheduler(pid, new PageScheduler(std::move(*req), disk_manager_, current_++));
+//       }
+//     } else {
+//       for (const auto &pair : page_shedulers_) {
+//         pair.second->Stop();
+//       }
+//       break;
+//     }
+//   }
+// }
+
+// void DiskScheduler::AddPageScheduler(page_id_t page_id, PageScheduler *page_scheduler) {
+//   outstanding_++;
+//   if (outstanding_ >= MAX_OUTSTANDING) {
+//     size_t timestamp = current_;
+//     std::vector<std::pair<page_id_t, PageScheduler *>> to_delete;
+//     for (const auto &pair : page_shedulers_) {
+//       if (pair.second->Join()) {
+//         to_delete.emplace_back(pair);
+//       } else if (pair.second->Compare(timestamp)) {
+//         pair.second->Stop();
+//       }
+//     }
+//     for (const auto &pair : to_delete) {
+//       page_shedulers_.erase(pair.first);
+//       delete (pair.second);
+//     }
+//     outstanding_ -= to_delete.size();
+//   }
+//   page_shedulers_[page_id] = page_scheduler;
+// }
+
+// PageScheduler::PageScheduler(DiskRequest req, DiskManager *disk_manager, size_t timestamp)
+//     : disk_manager_(disk_manager), timestamp_(timestamp) {
+//   if (req.is_write_) {
+//     writing_ = 1;
+//     last_write_ = req.data_;
+//   }
+//   disk_.Put(std::move(req));
+//   back_ = std::thread([&] { Disk(); });
+// }
+
+// void PageScheduler::Disk() {
+//   while (true) {
+//     auto req = disk_.Get();
+//     if (req.has_value()) {
+//       if (req->is_write_) {
+//         disk_manager_->WritePage(req->page_id_, req->data_);
+//         if (--writing_ == 0) {
+//           latch_.lock();
+//           last_write_ = nullptr;
+//           latch_.unlock();
+//         }
+//       } else {
+//         disk_manager_->ReadPage(req->page_id_, req->data_);
+//       }
+//       req->callback_.set_value(true);
+//     } else {
+//       break;
+//     }
+//   }
+// }
+
+// void PageScheduler::Schedule(DiskRequest req, size_t timestamp) {
+//   if (req.is_write_) {
+//     ++writing_;
+//     last_write_ = req.data_;
+//     disk_.Put(std::move(req));
+//     timestamp_ = timestamp;
+//   } else {
+//     latch_.lock();
+//     if (last_write_ == nullptr) {
+//       latch_.unlock();
+//       disk_.Put(std::move(req));
+//     } else {
+//       memcpy(req.data_, last_write_, BUSTUB_PAGE_SIZE);
+//       latch_.unlock();
+//       req.callback_.set_value(true);
+//     }
+//   }
+// }
+
+// auto PageScheduler::Compare(size_t &timestamp) -> bool {
+//   if (timestamp_ < timestamp) {
+//     timestamp = timestamp_;
+//     return true;
+//   }
+//   return false;
+// }
+
+// void PageScheduler::Stop() {
+//   if (!stop_) {
+//     disk_.Put(std::nullopt);
+//     stop_ = true;
+//   }
+// }
+
+// auto PageScheduler::Join() -> bool {
+//   if (stop_) {
+//     back_.join();
+//   }
+//   return stop_;
+// }
+// }  // namespace bustub
